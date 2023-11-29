d87767bf49250ae1043cbb132f7620cc79390ca7
Kalana Dananjaya <kalanadananjaya855@gmail.com>
2023-11-29 14:16:34 +0530

    for loop done

---
+++ FlowControlls/src/main/java/forLoop.java
@@ -0,0 +1,11 @@
+public class forLoop {
+    public static void main(String[] args) {
+        // Nested loops
+        /*for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 3; j++){
+                System.out.println(i+" "+j);
+            }
+        }*/
+
+    }
+}
