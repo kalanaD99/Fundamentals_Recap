58bc9bc0dcf3ec744c4b1a8cf4e33912b8eccd54
Kalana Dananjaya <kalanadananjaya855@gmail.com>
2023-11-29 14:25:44 +0530

    loops done

---
+++ FlowControlls/src/main/java/do_whileLoop.java
@@ -0,0 +1,10 @@
+public class do_whileLoop {
+    public static void main(String[] args) {
+        //eka parak body ek run wela tama condition ek balanne. condition ek true unath nethath body ek run wenaw
+        int i=0;
+        do {
+            System.out.println("doWhile loop "+i);
+            i++;
+        }while (i<10);
+    }
+}
---
+++ FlowControlls/src/main/java/while_Loop.java
@@ -0,0 +1,10 @@
+public class while_Loop {
+    public static void main(String[] args) {
+        // condition ek true nm body ek run wenawa
+        int i= 0;
+        while (i<10){
+            System.out.println("while loop"+i);
+            i++;
+        }
+    }
+}
